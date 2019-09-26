86f738fc4fd948f8691652f3d3791a2f181d65ec
Botmasher <languagejosh@hotmail.com>
2019-09-26 00:20:18 -1000

    Make 2.80 upgrade changes to ui and create PrettyImage class after api tests

--- pretty_img_2.8.py
+++ pretty_img_2.8.py
@@ -1,85 +1,98 @@
 #!/usr/bin/env python
 import bpy
 
-def load_scale_img (name, path, scale=1.0, channel=1, length=10, alpha=True):
-    scene = bpy.context.scene
-    scene.sequence_editor_create()
-    strip = scene.sequence_editor.sequences.new_image(name=name, filepath=path, channel=channel, frame_start=scene.frame_current)
+class PrettyImage():
+    def __init__(self):
+        self.scene = bpy.context.scene
+        self.sequencer = self.scene.sequence_editor
+        self.sequences = self.sequencer.sequences
 
-    def deselect_strips ():
-        bpy.ops.sequencer.select_all(action='DESELECT')
-        return
+    def deselect_strips(self):
+        selected_strips = []
+        for strip in self.sequences:
+            if strip.select:
+                selected_strips.append(strip)
+                strip.select = False
+        return selected_strips
 
-    # TODO: set channel strip wisely
-    # currently importing many causes interlaced stacking of transform and image strips
+    def load_scale_img(self, name, path, scale=1.0, channel=1, length=10, alpha=True):
+        self.scene.sequence_editor_create()
+        strip = self.sequences.new_image(
+            name=name,
+            filepath=path,
+            channel=channel,
+            frame_start=self.scene.frame_current
+            )
 
-    deselect_strips()
-    strip.select = True
-    scene.sequence_editor.active_strip = strip
+        # TODO: set channel strip wisely
+        # currently importing many causes interlaced stacking of transform and image strips
 
-    bpy.ops.sequencer.effect_strip_add(type='TRANSFORM')
-    transform_strip = scene.sequence_editor.active_strip
-    transform_strip.use_uniform_scale = False
+        self.deselect_strips()
+        strip.select = True
+        self.sequencer.active_strip = strip
 
-    # hack: update in viewport to read source img orig_width and orig_height
-    # switch view and area
-    area = bpy.context.area
-    original_area = area.type
-    area.type = 'SEQUENCE_EDITOR'
-    original_view = area.spaces[0].view_type
-    area.spaces[0].view_type = 'PREVIEW'
-    # NOTE playhead steps alone are sufficient when user has visible VSE Preview
-    frame_initial = scene.frame_current
-    scene.frame_current = strip.frame_start
-    bpy.ops.render.opengl(sequencer=True)
-    # reset view and area
-    area.spaces[0].view_type = original_view
-    area.type = original_area
-    # /hack
+        self.sequences.new_effect(name="Pretty Image", type='TRANSFORM')
+        transform_strip = self.sequencer.active_strip
+        transform_strip.use_uniform_scale = False
 
-    # gather image data
-    img = strip.elements[0]
-    # store dimensions
+        # hack: update in viewport to read source img orig_width and orig_height
+        # switch view and area
+        area = bpy.context.area
+        original_area = area.type
+        area.type = 'SEQUENCE_EDITOR'
+        original_view = area.spaces[0].view_type
+        area.spaces[0].view_type = 'PREVIEW'
+        # NOTE playhead steps alone are sufficient when user has visible VSE Preview
+        frame_initial = self.scene.frame_current
+        self.scene.frame_current = strip.frame_start
+        bpy.ops.render.opengl(sequencer=True)
+        # reset view and area
+        area.spaces[0].view_type = original_view
+        area.type = original_area
+        # /hack
 
-    if not (img.orig_width and img.orig_height):
-        print("pretty_img - Failed to rescale img with width or height of 0: {0}".format(img.filename))
-        return
+        # fetch and check image data
+        img = strip.elements[0]
+        if not (img.orig_width and img.orig_height):
+            print(f"pretty_img - Failed to rescale img with width or height of 0: {img.filename}")
+            return
 
-    img_res = {
-        'w': img.orig_width,
-        'h': img.orig_height
-    }
-    print("%s: %s" % (img.filename, "{0} x {1}".format(img_res['w'], img_res['h'])))
+        # store dimensions
+        img_res = {
+            'w': img.orig_width,
+            'h': img.orig_height
+        }
+        print(f"{img.filename}: {img_res['w']}, {img_res['h']}")
 
-    # resize image
-    # figure out how to resize just using input dimensions, the stretch applied, transform scale
-    render_res = {'w': scene.render.resolution_x, 'h': scene.render.resolution_y}
-    img_render_ratio = {'w': img_res['w'] / render_res['w'], 'h': img_res['h'] / render_res['h']}
-    print("Image vs screen res ratio - w: {0:.0f}%, h: {1:.0f}%".format(img_render_ratio['w'] * 100, img_render_ratio['h'] * 100))
+        # resize image
+        # figure out how to resize just using input dimensions, the stretch applied, transform scale
+        render_res = {'w': self.scene.render.resolution_x, 'h': self.scene.render.resolution_y}
+        img_render_ratio = {'w': img_res['w'] / render_res['w'], 'h': img_res['h'] / render_res['h']}
+        print(f"Image vs screen res ratio - w: {img_render_ratio['w'] * 100:.0f}%, h: {img_render_ratio['h'] * 100:.0f}%")
 
-    bpy.ops.sequencer.refresh_all()
+        bpy.ops.sequencer.refresh_all()
 
-    scaled_img_h = scene.render.resolution_y    # 1.0 scale y == 100% render_res.h
-    scaled_img_w = scene.render.resolution_x    # stretched value
-    scaled_img_w_target = img_res['w'] / img_res['h'] * scaled_img_h  # final value we're after
-    img_rescale_y = scaled_img_w_target / scaled_img_w  # what is that as a percentage of stretch?
+        scaled_img_h = self.scene.render.resolution_y    # 1.0 scale y == 100% render_res.h
+        scaled_img_w = self.scene.render.resolution_x    # stretched value
+        scaled_img_w_target = img_res['w'] / img_res['h'] * scaled_img_h  # final value we're after
+        img_rescale_y = scaled_img_w_target / scaled_img_w  # what is that as a percentage of stretch?
 
-    transform_strip.use_uniform_scale = False
-    transform_strip.scale_start_y = scale                   # w
-    transform_strip.scale_start_x = img_rescale_y * scale   # h
+        transform_strip.use_uniform_scale = False
+        transform_strip.scale_start_y = scale                   # w
+        transform_strip.scale_start_x = img_rescale_y * scale   # h
 
-    # set strip opacity
-    if alpha:
-        transform_strip.blend_type = 'ALPHA_OVER'
-        transform_strip.blend_alpha = 1.0
-        strip.blend_type = 'ALPHA_OVER'
-        strip.blend_alpha = 0.0
+        # set strip opacity
+        if alpha:
+            transform_strip.blend_type = 'ALPHA_OVER'
+            transform_strip.blend_alpha = 1.0
+            strip.blend_type = 'ALPHA_OVER'
+            strip.blend_alpha = 0.0
 
-    # set strip length
-    strip.frame_final_duration = length
+        # set strip length
+        strip.frame_final_duration = length
 
-    scene.frame_current = frame_initial
+        self.scene.frame_current = frame_initial
 
-    deselect_strips()
+        self.deselect_strips()
 
-    return strip
+        return strip
--- ui_2.8.py
+++ ui_2.8.py
@@ -1,20 +1,19 @@
 import bpy
-from bpy.props import BoolProperty, FloatProperty, IntProperty, CollectionProperty, StringProperty
 from bpy_extras.io_utils import ImportHelper
 
 # UI menu properties
 class PrettyImageProperties(bpy.types.PropertyGroup):
-    alpha: BoolProperty(
+    alpha : bpy.props.BoolProperty(
         name="Transparency",
         description="Use alpha blend on image transform",
         default=True
         )
-    scale: FloatProperty(
+    scale : bpy.props.FloatProperty(
         name="Scale",
         description="Transform scale to apply to fitted image",
         default=1.0
         )
-    length: IntProperty(
+    length : bpy.props.IntProperty(
         name="Strip length",
         description="Frame duration of imported images",
         default=10
@@ -26,6 +25,8 @@ class PrettyImagePanel (bpy.types.Panel):
     bl_idname = 'strip.pretty_image_loader'
     bl_space_type = 'SEQUENCE_EDITOR'
     bl_region_type = 'UI'
+    bl_category = 'Strip'
+    
     def draw (self, context):
         row = self.layout.row()
         row.operator("strip.pretty_strip_add", text="Add Pretty Image")
@@ -36,11 +37,11 @@ class PrettyImageOperator (bpy.types.Operator):
     bl_idname = 'strip.pretty_strip_add'
     bl_description = "Add a new image with alpha, transform strip and proper dimensions."
     # import settings
-    filepath = StringProperty (name='File Path')
-    files = CollectionProperty(name='File Names', type=bpy.types.OperatorFileListElement)
-    directory = StringProperty(maxlen=1024, subtype='DIR_PATH',options={'HIDDEN'})
-    filter_image = BoolProperty(default=True, options={'HIDDEN'})
-    filter_folder = BoolProperty(default=True, options={'HIDDEN'})
+    filepath = bpy.props.StringProperty (name='File Path')
+    files = bpy.props.CollectionProperty(name='File Names', type=bpy.types.OperatorFileListElement)
+    directory = bpy.props.StringProperty(maxlen=1024, subtype='DIR_PATH',options={'HIDDEN'})
+    filter_image = bpy.props.BoolProperty(default=True, options={'HIDDEN'})
+    filter_folder = bpy.props.BoolProperty(default=True, options={'HIDDEN'})
     # image loading/formatting
     set_alpha = PrettyImageProperties.alpha
     img_scale = PrettyImageProperties.scale
@@ -49,10 +50,9 @@ class PrettyImageOperator (bpy.types.Operator):
     def store_files (self, files):
         return [f.name for f in files]
 
-    def execute (self, ctx):
-        bpy.context.scene.sequence_editor_create()
+    def execute (self, context):
+        context.scene.sequence_editor_create()
         img_filenames = self.store_files(self.files)
-        img_path = self.directory
         [print(f) for f in img_filenames]
         return {'FINISHED'}
 
@@ -65,7 +65,8 @@ class PrettyImageOperator (bpy.types.Operator):
 class PrettyImageHandler (bpy.types.Operator):
     bl_label = "Pretty Image Handler"
     bl_idname = 'strip.pretty_strip_handler'
-    def execute (self, ctx):
+    
+    def execute (self, context):
         #bpy.ops.sequencer.pretty_strip_add('INVOKE_DEFAULT')
         print("Running image handler")
         return {'FINISHED'}
@@ -74,10 +75,25 @@ def menu_add(self, ctx):
     layout = self.layout
     layout.operator('strip.pretty_strip_handler', text="Pretty Strip")
 
+ui_classes = [
+    PrettyImageProperties,
+    PrettyImagePanel,
+    PrettyImageOperator
+]
+
 def register():
-    bpy.utils.register_class(PrettyImageProperties)
-    bpy.utils.register_class(PrettyImagePanel)
-    bpy.utils.register_class(PrettyImageOperator)
-    
+    for ui_class in ui_classes:
+        bpy.utils.register_class(ui_class)
+    bpy.types.SEQUENCER_MT_add.append(menu_add)
+    bpy.types.ImageSequence.pretty_image = bpy.props.PointerProperty(
+        type=PrettyImageProperties,
+        description=""
+        )
+
+def unregister():
+    for ui_class in reversed(ui_classes):
+        bpy.utils.unregister_class(ui_class) 
+    del bpy.types.ImageSequence.pretty_image
+
 if __name__ == '__main__':
     register()
